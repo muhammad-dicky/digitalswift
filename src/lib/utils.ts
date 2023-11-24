c302efd6f90dcd043bb5f2141f81d71e11a53dc7
Dicky <muhammad.dickynn@gmail.com>
2023-11-25 00:43:34 +0700

    Add Radix UI dependencies and implement user sign-in and cart functionality

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,25 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+// FUNCTION FOR FORMATTING PRICE TO CURRENCY
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency? : 'USD' | 'EUR' | 'GBP' | 'BDT'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+){
+  const {currency = 'USD', notation = 'compact'} = options
+
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price
+
+  return new Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
+
+}
