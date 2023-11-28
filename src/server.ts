e4122eeea7075632837661695034f35dce765c2d
Dicky <muhammad.dickynn@gmail.com>
2023-11-28 18:32:27 +0700

    Add Providers component to RootLayout

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express"
 import { getPayloadClient } from "./get-payload"
 import { nextApp, nextHandler } from "./next-utils"
+import { appRouter } from "./trpc"
+import * as trpcExpress from '@trpc/server/adapters/express'
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
+
+const createContext = ({req, res}: trpcExpress.CreateExpressContextOptions) => ({req, res,})
 
 const start = async () => {
     const payload = await getPayloadClient({
@@ -15,6 +19,13 @@ const start = async () => {
         },
     })
 
+    app.use(
+        '/api/trpc',
+        trpcExpress.createExpressMiddleware({
+            router: appRouter,
+            createContext,
+        })
+    )
 
     app.use((req, res) => nextHandler(req,res))
 
